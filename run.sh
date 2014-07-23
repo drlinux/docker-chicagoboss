fdf958fb0ab8aa4cb9c3f0c12c24af9a535cc47b
Ibrahim YILMAZ <prehistorya@gmail.com>
2014-07-24 02:41:30 +0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 clear
 if [[ $(id -u) -ne 0 ]] ; then echo "Please run as root" ; exit 1 ; fi
-/usr/bin/docker run -i -p 22 -p 8001 -v ~/development/richercart:/source/richercart -t drlinux/chicagoboss /bin/bash
+/usr/bin/docker run -i -p 8001 -v ~/development/richercart:/source/richercart -t drlinux/chicagoboss /bin/bash
