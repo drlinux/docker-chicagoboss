47502a277c5c22b67c4ff11c71730bcc5613e983
H. Ibrahim Yilmaz <ibrahim@drlinux.org>
2014-07-24 23:27:42 +0300

    add ssh & chicago boss ports

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 clear
 if [[ $(id -u) -ne 0 ]] ; then echo "Please run as root" ; exit 1 ; fi
-/usr/bin/docker run -i -p 8001 -v ~/development/richercart:/source/richercart -t drlinux/chicagoboss /bin/bash
+/usr/bin/docker run -i -d -p 8001:8001 -p 22:22 -t drlinux/chicagoboss && echo "Container succesfully started... Please visit 127.0.0.1:8001"
